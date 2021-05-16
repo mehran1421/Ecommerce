a084432eacfb0cf1aaca30094f66646cc3a64b31
mehran1421 <m.kamrani1421@gmail.com>
2021-05-16 15:05:28 +0430

    create cart app

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,6 +7,7 @@ ALLOWED_HOSTS = []
 THIRD_PARTY_APPS = [
     'products_app.apps.ProductsAppConfig',
     'account.apps.AccountConfig',
+    'cart.apps.CartsConfig',
 ]
 
 LOCAL_APPS = [
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products_app.urls')),
     path('auth/', include('account.urls')),
+    path('cart/', include('carts.urls')),
 
     path('api-auth/', include('rest_framework.urls')),
 ]
