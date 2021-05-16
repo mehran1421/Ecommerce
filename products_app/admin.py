c37ab0dd944e371416241b3cc8cf109ce074c99f
mehran1421 <m.kamrani1421@gmail.com>
2021-05-16 15:36:01 +0430

    update admin product app

--- products_app/admin.py
+++ products_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category, FormField, Images, Variation
+from .models import Product, Category, FormField, Images
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(FormField)
 admin.site.register(Images)
-admin.site.register(Variation)
+
