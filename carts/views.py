5c7926e8052b3f508b16aa2107082e34deb1d4a1
mehran1421 <m.kamrani1421@gmail.com>
2021-05-18 12:32:21 +0430

    caching in views

--- carts/views.py
+++ carts/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveDestroyAPIView
 from .serializers import CartSerializers, CartItemSerializers
 from .models import Cart, CartItem
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 
 class CartListApi(ListAPIView):
 	queryset = Cart.objects.all()
 	serializer_class = CartSerializers
+
+	@method_decorator(vary_on_cookie)
+	@method_decorator(cache_page(60 * 60))
+	def dispatch(self, *args, **kwargs):
+		return super(CartListApi, self).dispatch(*args, **kwargs)
 
 
 class CartListCreateApi(ListCreateAPIView):
@@ -18,6 +25,11 @@ class CartListCreateApi(ListCreateAPIView):
 
 	def perform_create(self, serializer):
 		return serializer.save(user=self.request.user)
+
+	@method_decorator(vary_on_cookie)
+	@method_decorator(cache_page(60 * 60))
+	def dispatch(self, *args, **kwargs):
+		return super(CartListCreateApi, self).dispatch(*args, **kwargs)
 
 
 class CartItemCreateApi(ListCreateAPIView):
@@ -43,5 +55,5 @@ class CartItemDeleteApi(RetrieveDestroyAPIView):
 	def perform_destroy(self, instance):
 		instance = self.get_object()
 		if instance.cart.user != self.request.user:
-			return Response("Cannot delete default system category",status=403)
+			return Response("Cannot delete default system category", status=403)
 		return instance.delete()
--- products_app/views.py
+++ products_app/views.py
@@ -1,47 +1,60 @@
 from django.shortcuts import render
 from rest_framework.generics import (
-    ListAPIView,
-    ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView,
+	ListAPIView,
+	ListCreateAPIView,
+	RetrieveUpdateDestroyAPIView,
 )
 from .serializers import (
-    ProductSerializer,
-    CategorySerializer,
-    ProductDetailSerializer
+	ProductSerializer,
+	CategorySerializer,
+	ProductDetailSerializer
 )
 from .models import (
-    Product,
-    Category
+	Product,
+	Category
 )
 from .pagination import CategoryPagination, ProductPagination
 from .permissions import IsSuperUserOrReadOnly
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 
 class ProductList(ListCreateAPIView):
-    serializer_class = ProductSerializer
-    queryset = Product.objects.all()
-    pagination_class = ProductPagination
-    permission_classes = (IsSuperUserOrReadOnly,)
+	serializer_class = ProductSerializer
+	queryset = Product.objects.all()
+	pagination_class = ProductPagination
+	permission_classes = (IsSuperUserOrReadOnly,)
+
+	@method_decorator(vary_on_cookie)
+	@method_decorator(cache_page(60 * 60))
+	def dispatch(self, *args, **kwargs):
+		return super(ProductList, self).dispatch(*args, **kwargs)
 
 
 class ProductDetail(RetrieveUpdateDestroyAPIView):
-    serializer_class = ProductDetailSerializer
-    lookup_field = 'slug'
-    queryset = Product.objects.all()
-    permission_classes = (IsSuperUserOrReadOnly,)
+	serializer_class = ProductDetailSerializer
+	lookup_field = 'slug'
+	queryset = Product.objects.all()
+	permission_classes = (IsSuperUserOrReadOnly,)
 
 
 class ProductCategory(ListAPIView):
-    serializer_class = ProductSerializer
-    pagination_class = ProductPagination
+	serializer_class = ProductSerializer
+	pagination_class = ProductPagination
 
-    def get_queryset(self):
-        category = Category.objects.get(slug=self.kwargs.get('slug'))
-        return Product.objects.filter(category=category)
+	def get_queryset(self):
+		category = Category.objects.get(slug=self.kwargs.get('slug'))
+		return Product.objects.filter(category=category)
 
 
 class CategoryList(ListCreateAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    pagination_class = CategoryPagination
-    permission_classes = (IsSuperUserOrReadOnly,)
+	serializer_class = CategorySerializer
+	queryset = Category.objects.all()
+	pagination_class = CategoryPagination
+	permission_classes = (IsSuperUserOrReadOnly,)
+
+	@method_decorator(vary_on_cookie)
+	@method_decorator(cache_page(60 * 60))
+	def dispatch(self, *args, **kwargs):
+		return super(CategoryList, self).dispatch(*args, **kwargs)
