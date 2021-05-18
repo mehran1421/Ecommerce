b8bc6dbdb2026e416e9139b2840489ee06b58dba
mehran1421 <m.kamrani1421@gmail.com>
2021-05-18 13:51:21 +0430

    create permission for cart app

---
+++ carts/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            # get access to superuser
+            request.user.is_authenticated and
+            request.user.is_superuser
+        )
+
+class IsSuperUserOrSelf(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.user.is_superuser or
+            obj.user==request.user
+        )
