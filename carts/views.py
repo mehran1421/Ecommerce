8d518687cb645267a504922647b0e149f0a00344
mehran1421 <m.kamrani1421@gmail.com>
2021-05-18 13:51:46 +0430

    use permissions in views

--- carts/views.py
+++ carts/views.py
@@ -5,55 +5,61 @@ from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
+from .permissions import IsSuperUser, IsSuperUserOrSelf
 
 
 class CartListApi(ListAPIView):
-	queryset = Cart.objects.all()
-	serializer_class = CartSerializers
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializers
+    permission_classes = (IsSuperUser,)
 
-	@method_decorator(vary_on_cookie)
-	@method_decorator(cache_page(60 * 60))
-	def dispatch(self, *args, **kwargs):
-		return super(CartListApi, self).dispatch(*args, **kwargs)
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(60 * 60))
+    def dispatch(self, *args, **kwargs):
+        return super(CartListApi, self).dispatch(*args, **kwargs)
 
 
 class CartListCreateApi(ListCreateAPIView):
-	serializer_class = CartSerializers
+    serializer_class = CartSerializers
+    permission_classes = (IsSuperUserOrSelf,)
 
-	def get_queryset(self):
-		return Cart.objects.filter(user=self.request.user)
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Cart.objects.all()
+        return Cart.objects.filter(user=self.request.user)
 
-	def perform_create(self, serializer):
-		return serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
 
-	@method_decorator(vary_on_cookie)
-	@method_decorator(cache_page(60 * 60))
-	def dispatch(self, *args, **kwargs):
-		return super(CartListCreateApi, self).dispatch(*args, **kwargs)
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(60 * 60))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CartListCreateApi, self).dispatch(*args, **kwargs)
 
 
 class CartItemCreateApi(ListCreateAPIView):
-	serializer_class = CartItemSerializers
+    serializer_class = CartItemSerializers
 
-	def get_queryset(self):
-		global cart
-		cart = Cart.objects.filter(user=self.request.user).first()
-		return CartItem.objects.filter(cart=cart)
+    def get_queryset(self):
+        global cart
+        cart = Cart.objects.filter(user=self.request.user).first()
+        return CartItem.objects.filter(cart=cart)
 
-	def perform_create(self, serializer):
-		return serializer.save(cart=cart)
+    def perform_create(self, serializer):
+        return serializer.save(cart=cart)
 
 
 class CartItemDeleteApi(RetrieveDestroyAPIView):
-	serializer_class = CartItemSerializers
-	lookup_field = 'pk'
+    serializer_class = CartItemSerializers
+    lookup_field = 'pk'
+    permission_classes = (IsSuperUserOrSelf,)
 
-	def get_object(self):
-		queryset = CartItem.objects.filter(id=self.kwargs['pk']).first()
-		return queryset
+    def get_object(self):
+        queryset = CartItem.objects.filter(id=self.kwargs['pk']).first()
+        return queryset
 
-	def perform_destroy(self, instance):
-		instance = self.get_object()
-		if instance.cart.user != self.request.user:
-			return Response("Cannot delete default system category", status=403)
-		return instance.delete()
+    def perform_destroy(self, instance):
+        instance = self.get_object()
+        if instance.cart.user != self.request.user:
+            return Response("Cannot delete default system category", status=403)
+        return instance.delete()
