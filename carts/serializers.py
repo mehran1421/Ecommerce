baabad1983e7d53320a6a5ecabf4e943fc3474bb
mehran1421 <m.kamrani1421@gmail.com>
2021-05-19 11:49:03 +0430

    add link pay in to cart serializer

--- carts/serializers.py
+++ carts/serializers.py
@@ -4,31 +4,35 @@ from products_app.serializers import ProductSerializer
 
 
 class CartItemSerializers(ModelSerializer):
-	delete = HyperlinkedIdentityField(view_name='cart:delete-cartItem', lookup_field='pk')
+    delete = HyperlinkedIdentityField(view_name='cart:delete-cartItem', lookup_field='pk')
 
-	class Meta:
-		model = CartItem
-		fields = [
-			'cart',
-			'delete',
-			'item',
-			'quantity',
-			'line_item_total',
-		]
+    class Meta:
+        model = CartItem
+        fields = [
+            'cart',
+            'delete',
+            'item',
+            'quantity',
+            'line_item_total',
+        ]
 
 
 class CartSerializers(ModelSerializer):
-	products = ProductSerializer(many=True)
-	cart_item = CartItemSerializers(source='cartitem_set', many=True)
+    products = ProductSerializer(many=True)
+    cart_item = CartItemSerializers(source='cartitem_set', many=True)
+    # first add MERCHANT in payment app
+    # must connect to internet 
+    pay = HyperlinkedIdentityField(view_name='pay:request')
 
-	class Meta:
-		model = Cart
-		fields = [
-			'user',
-			'cart_item',
-			'products',
-			'subtotal',
-			'total',
-			'timestamp',
-			'updated',
-		]
+    class Meta:
+        model = Cart
+        fields = [
+            'user',
+            'cart_item',
+            'pay',
+            'products',
+            'subtotal',
+            'total',
+            'timestamp',
+            'updated',
+        ]
--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'pay'
 urlpatterns = [
     path('request/', views.send_request, name='request'),
     path('verify/<int:pk>/', views.verify, name='verify'),
