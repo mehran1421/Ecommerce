3a36ac0fe4c3d8e2ca3523e6c9732a4b73eda283
mehran1421 <m.kamrani1421@gmail.com>
2021-05-21 17:19:17 +0430

    list and retrieve products in viewsets

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 from rest_framework.generics import (
     ListAPIView,
     ListCreateAPIView,
@@ -14,7 +15,7 @@ from .models import (
     Category
 )
 from .pagination import PaginationTools
-from .permissions import IsSuperUserOrReadOnly
+from .permissions import IsSuperUserOrIsSellerOrReadOnly, IsSuperUserOrIsSellerProduct
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
@@ -24,7 +25,7 @@ class ProductList(ListCreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     pagination_class = PaginationTools
-    permission_classes = (IsSuperUserOrReadOnly,)
+    permission_classes = (IsSuperUserOrIsSellerOrReadOnly,)
 
     @method_decorator(vary_on_cookie)
     @method_decorator(cache_page(60 * 60))
@@ -36,7 +37,7 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = ProductDetailSerializer
     lookup_field = 'slug'
     queryset = Product.objects.all()
-    permission_classes = (IsSuperUserOrReadOnly,)
+    permission_classes = (IsSuperUserOrIsSellerProduct,)
 
 
 class ProductCategory(ListAPIView):
@@ -52,9 +53,39 @@ class CategoryList(ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
     pagination_class = PaginationTools
-    permission_classes = (IsSuperUserOrReadOnly,)
+    permission_classes = (IsSuperUserOrIsSellerProduct,)
 
     @method_decorator(vary_on_cookie)
     @method_decorator(cache_page(60 * 60))
     def dispatch(self, *args, **kwargs):
         return super(CategoryList, self).dispatch(*args, **kwargs)
+
+
+class ProductViews(ViewSet):
+    def get_permissions(self):
+        if self.action['create']:
+            return IsSuperUserOrIsSellerOrReadOnly
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(vary_on_cookie)
+    def list(self, request):
+        queryset = Product.objects.filter(status=True, choice='p')
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, slug=None):
+        queryset = Product.objects.filter(slug=slug, status=True, choice='p')
+        serializer = ProductDetailSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
