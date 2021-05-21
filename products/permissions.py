071f308c3944c15eb0abad6e58e47bd246e599d5
mehran1421 <m.kamrani1421@gmail.com>
2021-05-21 15:39:23 +0430

    add permission for delete or update product By owner product or super user

--- products/permissions.py
+++ products/permissions.py
@@ -1,7 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsSuperUserOrReadOnly(BasePermission):
+class IsSuperUserOrIsSellerOrReadOnly(BasePermission):
+    """
+    for create product,
+    just superuser or seller
+    """
+
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
@@ -9,5 +14,19 @@ class IsSuperUserOrReadOnly(BasePermission):
         return bool(
             # get access to superuser
             request.user.is_authenticated and
-            request.user.is_superuser
+            request.user.is_superuser or
+            request.user.is_seller
+        )
+
+
+class IsSuperUserOrIsSellerProduct(BasePermission):
+    """
+    for delete or update product,just superuser or product seller
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.user.is_authenticated and
+            request.user.is_superuser or
+            obj.seller == request.user
         )
