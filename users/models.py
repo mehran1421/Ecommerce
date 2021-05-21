df36249f26f159defe4902051bfbe778f052f198
mehran1421 <m.kamrani1421@gmail.com>
2021-05-21 15:19:58 +0430

    add is_seller field in users app

--- users/models.py
+++ users/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='ایمیل')
+    is_seller=models.BooleanField(default=False,verbose_name='آیا کاربر فروشنده است')
+
