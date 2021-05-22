398ee98b4e47d7418220993ad19d8d447553cc45
mehran1421 <m.kamrani1421@gmail.com>
2021-05-23 01:05:17 +0430

    add CartList and CartItemList serializers in serializers.py

--- carts/serializers.py
+++ carts/serializers.py
@@ -3,23 +3,47 @@ from .models import Cart, CartItem
 from products.serializers import ProductSerializer
 
 
-class CartItemSerializers(ModelSerializer):
-    delete = HyperlinkedIdentityField(view_name='cart:delete-cartItem', lookup_field='pk')
-
+class CartItemListSerializers(ModelSerializer):
     class Meta:
         model = CartItem
         fields = [
             'cart',
-            'delete',
             'item',
             'quantity',
             'line_item_total',
         ]
 
 
-class CartSerializers(ModelSerializer):
+class CartItemDetailSerializers(ModelSerializer):
+    # delete = HyperlinkedIdentityField(view_name='cart:delete-cartItem', lookup_field='pk')
+
+    class Meta:
+        model = CartItem
+        fields = [
+            'cart',
+            # 'delete',
+            'item',
+            'quantity',
+            'line_item_total',
+        ]
+
+
+class CartListSerializers(ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = [
+            'user',
+            'subtotal',
+            'total',
+            'timestamp',
+            'updated',
+        ]
+
+
+class CartDetailSerializers(ModelSerializer):
     products = ProductSerializer(many=True)
-    cart_item = CartItemSerializers(source='cartitem_set', many=True)
+    cart_item = CartItemListSerializers(source='cartitem_set', many=True)
+
     # first add MERCHANT in payment app
     # must connect to internet
     # pay = HyperlinkedIdentityField(view_name='pay:request')
