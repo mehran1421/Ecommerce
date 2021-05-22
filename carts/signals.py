6ecf794a73e5f4f3210c117b18a1d5dc5057aa0e
mehran1421 <m.kamrani1421@gmail.com>
2021-05-23 01:00:07 +0430

    create signals.py for signals in carts app

---
+++ carts/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import pre_save, post_save, post_delete
+from django.dispatch import receiver
+from .models import CartItem
+from decimal import Decimal
+
+
+@receiver(pre_save, sender=CartItem)
+def cart_item_pre_save_receiver(sender, instance, *args, **kwargs):
+    qty = instance.quantity
+    if qty >= 1:
+        price = instance.item.price
+        line_item_total = Decimal(qty) * Decimal(price)
+        instance.line_item_total = line_item_total
+
+
+@receiver(post_save, sender=CartItem)
+@receiver(post_delete, sender=CartItem)
+def cart_item_post_save_receiver(sender, instance, *args, **kwargs):
+    instance.cart.update_subtotal()
