d6b91c5df3a8def45a45eb8629930d0a7b99fc2e
mehran1421 <m.kamrani1421@gmail.com>
2021-05-23 13:24:35 +0430

    add cartview to router urls.py

--- carts/urls.py
+++ carts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CartItemViews
+from .views import CartItemViews,CartViews
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'cartItem', CartItemViews, basename='cart-item')
+router.register(r'cart', CartViews, basename='cart')
 
 # The API URLs are now determined automatically by the router.
 app_name = 'carts'
