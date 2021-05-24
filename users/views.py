f4a0e44f730f6097fe1297c10a16d5e3aee0a041
mehran1421 <m.kamrani1421@gmail.com>
2021-05-24 21:37:08 +0430

    retreive view for user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from extension.cacheOps import cacheops
+from django.contrib.auth import get_user_model
+from carts.permissions import IsSuperUser
+from .serializers import (
+    UserListSerializers,
+    UserDetailSerializers
+)
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserViews(ViewSet):
+    permission_classes = (IsSuperUser,)
+
+    lookup_field = 'username'
+
+    def list(self, request):
+        obj = cacheops(request, 'user-list', User)
+        serializer = UserListSerializers(obj, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, username=None):
+        obj = cacheops(request, 'user-list', User)
+        queryset = obj.filter(username=username)
+        serializer = UserDetailSerializers(queryset, context={'request': request}, many=True)
+        return Response(serializer.data)
