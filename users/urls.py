6c37ed0c4bafc9b5a623d9d4b485ada32f080d19
mehran1421 <m.kamrani1421@gmail.com>
2021-05-24 21:37:28 +0430

    router url

--- users/urls.py
+++ users/urls.py
@@ -1,2 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViews
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'users', UserViews, basename='user')
+
+# The API URLs are now determined automatically by the router.
+app_name = 'user'
 urlpatterns = [
+    path('', include(router.urls)),
 ]
