3c5d1c786ac206bccdc131036a384b6f25ae7543
mehran1421 <m.kamrani1421@gmail.com>
2021-05-24 21:40:19 +0430

    add detail url in UserListSerializers

--- users/serializers.py
+++ users/serializers.py
@@ -9,9 +9,12 @@ User = get_user_model()
 
 
 class UserListSerializers(ModelSerializer):
+    url = HyperlinkedIdentityField(view_name='user:user-detail', lookup_field='username')
+
     class Meta:
         model = User
         fields = [
+            'url',
             'username',
             'email',
             'is_seller'
