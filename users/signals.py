3cc06c344a74a2be23264668ba673d00515fe728
mehran1421 <m.kamrani1421@gmail.com>
2021-05-24 21:56:07 +0430

    settings cache in users app

--- users/signals.py
+++ users/signals.py
@@ -1,11 +1,11 @@
 from django.core.cache import cache
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import post_delete, post_save
 
 User = get_user_model()
 
 
-@receiver([pre_save, pre_delete], sender=User)
-def pre_delete_receiver_product(sender, instance, *args, **kwargs):
+@receiver([post_delete, post_save], sender=User)
+def pre_delete_receiver_user(sender, instance, *args, **kwargs):
     cache.delete('user-list')
