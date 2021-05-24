345912e100d3bbebbcb415b8ba7f937e60414fdb
mehran1421 <m.kamrani1421@gmail.com>
2021-05-24 17:45:13 +0430

    urls for payment views

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PayViews
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'pay', PayViews, basename='payment')
+
+# The API URLs are now determined automatically by the router.
 app_name = 'pay'
 urlpatterns = [
     path('request/', views.send_request, name='request'),
     path('verify/<int:pk>/', views.verify, name='verify'),
+    path('', include(router.urls)),
 ]
