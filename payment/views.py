86bbfda4f1bf137015cfc0ac1cc8952d17e70d85
mehran1421 <m.kamrani1421@gmail.com>
2021-05-24 17:44:32 +0430

    list payment cart (is_pay=True)

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
 from carts.models import Cart
+from carts.serializers import CartListSerializers, CartDetailSerializers
+from carts.permissions import IsSuperUser
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from zeep import Client
 from datetime import datetime
+from django.core.cache import cache
 from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
 MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
 client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
@@ -13,6 +16,21 @@ description = "توضیحات مربوط به تراکنش را در این قس
 email = 'email@example.com'  # Optional
 mobile = '09123456789'  # Optional
 CallbackURL = 'http://localhost:8000/verify'  # Important: need to edit for realy server.
+
+
+class PayViews(ViewSet):
+    permission_classes = (IsSuperUser,)
+
+    def list(self, request):
+        obj = cache.get('cart-list', None)
+        if obj is None:
+            obj = Cart.objects.all()
+            cache.set('cart-list', obj)
+        cart = obj.filter(is_pay=True)
+        serializer = CartListSerializers(cart, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    
 
 
 def send_request(request):
