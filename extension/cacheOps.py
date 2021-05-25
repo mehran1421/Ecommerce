bf6979a0f5670066c3acfe9509bf96ea5e6bdbce
mehran1421 <m.kamrani1421@gmail.com>
2021-05-25 11:42:39 +0430

    delete extension/cacheops and write function cacheops in extnsion/utils.py

--- extension/cacheOps.py
+++
@@ -1,9 +0,0 @@
-from django.core.cache import cache
-
-
-def cacheops(request, name, model):
-    obj = cache.get(name, None)
-    if obj is None:
-        obj = model.objects.all()
-        cache.set(name, obj)
-    return obj
