ca09ee9098d922cd1b4caa01829aaae1259afd20
mehran1421 <m.kamrani1421@gmail.com>
2021-05-27 15:04:21 +0430

    delete utils.py in products app and use utils.py in extension directory

--- extension/utils.py
+++ extension/utils.py
@@ -1,6 +1,9 @@
+import random
+import string
 from . import jalali
 from django.utils import timezone
 from django.core.cache import cache
+from django.utils.text import slugify
 
 
 def persian_number_converter(mystr):
@@ -51,3 +54,34 @@ def cacheops(request, name, model):
         obj = model.objects.all()
         cache.set(name, obj)
     return obj
+
+
+'''
+random_string_generator is located here:
+http://joincfe.com/blog/random-string-generator-in-python/
+'''
+
+
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unique_slug_generator(instance, new_slug=None):
+    """
+    This is for a Django project and it assumes your instance
+    has a model with a slug field and a title character (char) field.
+    """
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(slug=slug).exists()
+    if qs_exists:
+        new_slug = "{slug}-{randstr}".format(
+            slug=slug,
+            randstr=random_string_generator(size=4)
+        )
+        return unique_slug_generator(instance, new_slug=new_slug)
+    return slug
