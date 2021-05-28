e5674694ad693ed85c6efee9ba83d965730faa83
mehran1421 <m.kamrani1421@gmail.com>
2021-05-28 18:17:05 +0430

    add database caching for products

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,7 +136,11 @@ CACHES = {
     "default": {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
-        },
+    },
+    'products': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'products_status_True',
+    }
 }
 
 # Password validation
