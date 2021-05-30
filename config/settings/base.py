eae2476b2113ee2b3c843914ec2286548d7219c4
mehran1421 <m.kamrani1421@gmail.com>
2021-05-30 23:07:32 +0430

    delete CACHE in settings.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,18 +132,6 @@ JWT_AUTH_REFRESH_COOKIE = 'refresh'
 
 CUSTOM_PASSWORD_RESET_CONFIRM = 'desired URL'
 
-CACHES = {
-    "default": {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'TIMEOUT': 60 * 60 * 24
-        },
-    }
-
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
