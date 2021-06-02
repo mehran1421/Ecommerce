d99e0ddb38bf9949ce1c6d396124122180422b94
mehran1421 <m.kamrani1421@gmail.com>
2021-06-02 15:08:31 +0430

    delete cache when change products

--- items/signals.py
+++ items/signals.py
@@ -1,7 +1,7 @@
 from extension.utils import unique_slug_generator
-from django.core.cache import caches
+from django.core.cache import caches, cache
 from django.dispatch import receiver
-from .models import Product, Category
+from .models import Product, Category, FigureField
 from carts.models import CartItem
 from django.db.models.signals import (
     pre_save,
@@ -26,3 +26,19 @@ def pre_save_receiver(sender, instance, *args, **kwargs):
             i.save()
     except Exception:
         pass
+    caches['all_products'].delete('products')
+
+
+@receiver(pre_delete, sender=Product)
+def pre_delete_receiver_product(sender, instance, *args, **kwargs):
+    caches['all_products'].delete('products')
+
+
+@receiver([pre_delete, pre_save], sender=Category)
+def pre_delete_receiver_category(sender, instance, *args, **kwargs):
+    cache.delete('category')
+
+
+@receiver([pre_delete, pre_save], sender=FigureField)
+def pre_delete_receiver_category(sender, instance, *args, **kwargs):
+    cache.delete('figure')
