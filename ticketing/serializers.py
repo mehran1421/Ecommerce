17712b7deaccc6574e7caecbfcc167fc5a2b3f86
mehran1421 <m.kamrani1421@gmail.com>
2021-06-22 20:05:15 +0430

    add serializers.py

---
+++ ticketing/serializers.py
@@ -0,0 +1,79 @@
+from rest_framework.serializers import (
+    ModelSerializer,
+    HyperlinkedIdentityField,
+    SerializerMethodField
+)
+from .models import (
+    Ticket,
+    Answare
+)
+from users.serializers import UserListSerializers
+
+
+class TicketListSerializer(ModelSerializer):
+    # detail = HyperlinkedIdentityField(view_name='product:figure-detail')
+
+    class Meta:
+        model = Ticket
+        fields = [
+            'user',
+            'create',
+            'title',
+            'status',
+        ]
+
+
+class TicketDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = [
+            'user',
+            'description',
+            'create',
+            'title',
+            'status',
+        ]
+
+
+class TicketCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = [
+            'user',
+            'description',
+            'title',
+            'status',
+        ]
+
+
+class AnswerListSerializer(ModelSerializer):
+    # detail = HyperlinkedIdentityField(view_name='product:figure-detail')
+
+    class Meta:
+        model = Answare
+        fields = [
+            'user',
+            'create',
+            'ticket',
+        ]
+
+
+class AnswerDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Answare
+        fields = [
+            'user',
+            'description',
+            'create',
+            'status',
+        ]
+
+
+class AnswerCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Answare
+        fields = [
+            'user',
+            'description',
+            'status',
+        ]
