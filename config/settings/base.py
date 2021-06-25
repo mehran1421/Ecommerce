b7466f51bc08e300c3bba1b07c80fdbc8358b5df
mehran1421 <m.kamrani1421@gmail.com>
2021-06-25 15:01:19 +0430

    throttling security

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,6 +127,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/day',
+        'user': '100/day'
+    }
 }
 
 REST_AUTH_SERIALIZERS = {
