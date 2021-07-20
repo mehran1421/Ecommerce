4c19da626c322080e4e9a33badc1b5846a2b6743
mehran1421 <m.kamrani1421@gmail.com>
2021-07-21 01:33:14 +0430

    commenting cart app

--- carts/models.py
+++ carts/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 from users.models import User
 from items.models import Product
-from django.db.models.signals import pre_save, post_save, post_delete
 
 
 class Cart(models.Model):
     """
     Store a cart belonging to a user.
+    if user pay Cart ===> is_pay = True
+    in payment app, it has been exported Factor that is_pay = True
+    user can just cart objects that is_pay = False in carts app
+    but in payment app show him all cart object is_pay = True
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
     products = models.ManyToManyField(Product, through='CartItem', verbose_name='محصول')
+
+    # sum of all line_item_total in CartItem
+    # for example CartItem([line_item_total=100],[line_item_total=200] ===> subtotal = 300
     subtotal = models.DecimalField(default=0.00, null=True, blank=True, max_digits=100, decimal_places=2,
                                    verbose_name='مجموع قیمت')
+
+    # count of CartItem
+    # for example CartItem([line_item_total=100],[line_item_total=200] ===> total = 2
     total = models.PositiveSmallIntegerField(blank=True, null=True, default=0, verbose_name='تعداد')
     timestamp = models.DateTimeField(auto_now_add=True, verbose_name='زمان اضاقه شدن')
     updated = models.DateTimeField(auto_now=True, verbose_name='زمان اپدیت')
+
+    # for know that is pay cart or no
     is_pay = models.BooleanField(default=False, verbose_name="آیا پرداخت شده")
 
     class Meta:
@@ -28,18 +39,28 @@ class Cart(models.Model):
         return str(self.user.get_full_name()) + " : " + str(self.user.username)
 
     def update_subtotal(self):
+        """
+        update subtotal in cart object
+        each create or delete object in cartItem or product, call this functions
+        :return:
+        """
         subtotal = 0
         items = self.cartitem_set.all()
         for item in items:
             subtotal += item.line_item_total
-        self.subtotal = "%.2f" % (subtotal)
+        self.subtotal = "%.2f" % subtotal
         self.save()
 
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True, verbose_name='سبد خرید')
     item = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, verbose_name='محصول')
+
+    # count of want product
     quantity = models.PositiveIntegerField(default=1)
+
+    # sum of price product
+    # for example product(price=100) and quantity is 4 ==> line_item_total = 400
     line_item_total = models.DecimalField(default=0.00, null=True, blank=True, max_digits=10, decimal_places=2)
 
     def __str__(self):
--- carts/views.py
+++ carts/views.py
@@ -24,9 +24,17 @@ from .models import (
 class CartItemViews(ViewSet):
     """
     users can list,retrieve,update,destroy,create object for each cart
+    use caching with redis and database cache
+    cacheCart ==> extension/utils.py ==> cache in redis by email user
+    cacheCartItem ==> extension/utils.py ==> cache in redis by email user
+
     """
 
     def get_permissions(self):
+        """
+        IsSuperUserOrOwnerCart ==> if cartItem object have request.user or user is superuser
+        :return:
+        """
         if self.action in ['update', 'destroy']:
             permission_classes = (IsSuperUserOrOwnerCart,)
         else:
@@ -36,12 +44,17 @@ class CartItemViews(ViewSet):
     def list(self, request):
         """
         other user can just see self cartItems object
+        show user CartItem list for your cart
         :param request:
         :return:
         """
         try:
             obj = cacheCart(request, f'cart-{request.user.email}', Cart, request.user)
+
+            # this is for select is_pay = False object, that means thay that dont payment
             cart_obj = obj.filter(is_pay=False).first()
+
+            # this is for select CartItem objects that are in user cart that is_pay = False
             query = cacheCartItem(request, f'cartItem-{cart_obj.user.email}', CartItem, cart_obj)
             serializer = CartItemListSerializers(query, context={'request': request}, many=True)
             return response.SuccessResponse(serializer.data).send()
@@ -57,8 +70,12 @@ class CartItemViews(ViewSet):
         """
         try:
             obj = cacheCart(request, f'cart-{request.user.email}', Cart, request.user)
+
+            # this is for select is_pay = False object, that means thay that dont payment
             cart_obj = obj.get(is_pay=False)
             cart_items = cacheCartItem(request, f'cartItem-{cart_obj.user.email}', CartItem, cart_obj)
+
+            # this is for select CartItem object that is in user cart that is_pay = False
             queryset = cart_items.get(pk=pk)
             serializer = CartItemDetailSerializers(queryset, context={'request': request})
             return response.SuccessResponse(serializer.data).send()
@@ -68,8 +85,12 @@ class CartItemViews(ViewSet):
     def destroy(self, request, pk=None):
         try:
             obj = cacheCart(request, f'cart-{request.user.email}', Cart, request.user)
+
+            # this is for select is_pay = False object, that means thay that dont payment
             cart = obj.get(is_pay=False)
             cart_items = cacheCartItem(request, f'cartItem-{cart.user.email}', CartItem, cart)
+
+            # this is for select CartItem objects that are in user cart that is_pay = False
             queryset = cart_items.get(pk=pk)
 
             queryset.delete()
@@ -80,11 +101,14 @@ class CartItemViews(ViewSet):
     def create(self, request):
         """
         when create owner user is request.user and is_pay=False
+        create CartItem object for your cart that is_pay = False
         :param request:
         :return:
         """
         try:
             serializer = CartItemInputSerializers(data=request.data)
+
+            # get user cart for add created object to your cart
             obj_cart = cacheCart(request, f'cart-{request.user.email}', Cart, request.user)
             cart = obj_cart.get(is_pay=False)
             if serializer.is_valid():
@@ -115,6 +139,10 @@ class CartItemViews(ViewSet):
 
 class CartViews(ViewSet):
     def get_permissions(self):
+        """
+        IsSuperUserOrOwnerCart ==> if cartItem object have request.user or user is superuser
+        :return:
+        """
         if self.action in ['retrieve', 'destroy', 'update']:
             permission_classes = (IsSuperUserOrOwnerCart,)
         else:
@@ -124,6 +152,8 @@ class CartViews(ViewSet):
     def list(self, request):
         """
                other user can show list carts that is_pay=False
+               show user just one cart that is_pay = False
+               Indeed, user cant create more than one cart
                :param request:
                :return:
         """
