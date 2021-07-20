75b6446aa29287bc498a554c0be55312d5115c1a
mehran1421 <m.kamrani1421@gmail.com>
2021-07-21 02:07:44 +0430

    commenting model ticketing app

--- ticketing/models.py
+++ ticketing/models.py
@@ -3,6 +3,10 @@ from users.models import User
 
 
 class TicketCreatorTime(models.Model):
+    """
+    abstract class for inheritance
+    user and create is Common in Ticket and QuestionAndAnswer
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     create = models.DateTimeField(auto_now_add=True)
 
@@ -11,6 +15,11 @@ class TicketCreatorTime(models.Model):
 
 
 class Ticket(TicketCreatorTime):
+    """
+    if status == cl ==> ticket is close and can not create or update it
+    elif status == de ==> ticket is Waiting for answer superuser
+    elif status == bs ==> ticket Reviewed by superuser and Wait user that answer him
+    """
     STATUS_CHOICE = (
         ('cl', 'بسته شده'),
         ('de', 'در انتظار'),
@@ -28,6 +37,10 @@ class Ticket(TicketCreatorTime):
 
 
 class QuestionAndAnswer(TicketCreatorTime):
+    """
+    each Ticket have many QuestionAndAnswer object
+    that mean Ticket ==>([QA1,QA2,...]) that each QA object are for superuser or user
+    """
     description = models.TextField()
     question = models.ForeignKey(Ticket, on_delete=models.CASCADE)
 
