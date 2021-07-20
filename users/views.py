7e07bfeb9245d599c2f98055b8baee21b1ce0339
mehran1421 <m.kamrani1421@gmail.com>
2021-07-20 23:58:38 +0430

    use my response and coustom exception in users/views.py

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from extension.permissions import IsSuperUserOrOwnerCart
+from extension.exception import CustomException
+from extension import response
 from .serializers import (
     UserListSerializers,
     UserDetailSerializers
@@ -16,12 +18,18 @@ class UserViews(ViewSet):
     lookup_field = 'username'
 
     def list(self, request):
-        obj = User.objects.all()
-        serializer = UserListSerializers(obj, context={'request': request}, many=True)
-        return Response(serializer.data)
+        try:
+            obj = User.objects.all()
+            serializer = UserListSerializers(obj, context={'request': request}, many=True)
+            return response.SuccessResponse(serializer.data).send()
+        except CustomException as e:
+            return response.ErrorResponse(message=e.detail, status=e.status_code).send()
 
     def retrieve(self, request, username=None):
-        obj = User.objects.all()
-        queryset = obj.filter(username=username)
-        serializer = UserDetailSerializers(queryset, context={'request': request}, many=True)
-        return Response(serializer.data)
+        try:
+            obj = User.objects.all()
+            queryset = obj.filter(username=username)
+            serializer = UserDetailSerializers(queryset, context={'request': request}, many=True)
+            return response.SuccessResponse(serializer.data).send()
+        except CustomException as e:
+            return response.ErrorResponse(message=e.detail, status=e.status_code).send()
